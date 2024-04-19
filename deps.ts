a3031f1197c0e244e0773aa4d5fe83d54c4010f0
aucub <78630225+aucub@users.noreply.github.com>
2024-04-19 17:10:34 +0000

    bump deno.land/x/openai from v4.38.0 to v4.38.1

--- deps.ts
+++ deps.ts
@@ -7,7 +7,7 @@ export {
   createMiddleware,
   env,
   streamSSE,
-  testClient
+  testClient,
 } from "https://deno.land/x/hono@v4.2.5/helper.ts";
 export {
   compress,
@@ -15,16 +15,16 @@ export {
   logger,
   prettyJSON,
   secureHeaders,
-  timing
+  timing,
 } from "https://deno.land/x/hono@v4.2.5/middleware.ts";
-export { HTTPException, Hono } from "https://deno.land/x/hono@v4.2.5/mod.ts";
+export { Hono, HTTPException } from "https://deno.land/x/hono@v4.2.5/mod.ts";
 export type { MiddlewareHandler } from "https://deno.land/x/hono@v4.2.5/types.ts";
-export { OpenAI } from "https://deno.land/x/openai@v4.38.0/mod.ts";
-export { ZodSchema, z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+export { OpenAI } from "https://deno.land/x/openai@v4.38.1/mod.ts";
+export { z, ZodSchema } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 export { Ai } from "https://esm.sh/@cloudflare/ai@1.1.0";
 export {
   HarmBlockThreshold,
-  HarmCategory
+  HarmCategory,
 } from "https://esm.sh/@google/generative-ai@0.7.1";
 export { zValidator } from "https://esm.sh/@hono/zod-validator@0.2.1";
 export { ChatCloudflareWorkersAI } from "https://esm.sh/@langchain/cloudflare@0.0.4";
@@ -32,43 +32,47 @@ export type { CloudflareWorkersAIInput } from "https://esm.sh/@langchain/cloudfl
 export { HuggingFaceInferenceEmbeddings } from "https://esm.sh/@langchain/community@0.0.50/embeddings/hf";
 export { Document } from "https://esm.sh/@langchain/core@0.1.58/documents";
 export type { BaseLanguageModelInput } from "https://esm.sh/@langchain/core@0.1.58/language_models/base";
-export type { BaseChatModelParams } from "https://esm.sh/@langchain/core@0.1.58/language_models/chat_models";
+export type {
+  BaseChatModelCallOptions,
+  BaseChatModelParams,
+} from "https://esm.sh/@langchain/core@0.1.58/language_models/chat_models";
 export {
   AIMessage,
   BaseMessage,
-  HumanMessage, SystemMessage, isBaseMessage,
-  isBaseMessageChunk
+  HumanMessage,
+  isBaseMessage,
+  isBaseMessageChunk,
+  SystemMessage,
 } from "https://esm.sh/@langchain/core@0.1.58/messages";
 export type {
   BaseMessageChunk,
-  BaseMessageLike
+  BaseMessageLike,
 } from "https://esm.sh/@langchain/core@0.1.58/messages";
 export { OutputParserException } from "https://esm.sh/@langchain/core@0.1.58/output_parsers";
 export { ToolInputParsingException } from "https://esm.sh/@langchain/core@0.1.58/tools";
 export { IterableReadableStream } from "https://esm.sh/@langchain/core@0.1.58/utils/stream";
 export {
   ChatGoogleGenerativeAI,
-  GoogleGenerativeAIEmbeddings
+  GoogleGenerativeAIEmbeddings,
 } from "https://esm.sh/@langchain/google-genai@0.0.11";
 export type {
   GoogleGenerativeAIChatInput,
-  GoogleGenerativeAIEmbeddingsParams
+  GoogleGenerativeAIEmbeddingsParams,
 } from "https://esm.sh/@langchain/google-genai@0.0.11";
 export {
   ChatOpenAI,
   DallEAPIWrapper,
-  OpenAIEmbeddings
+  OpenAIEmbeddings,
 } from "https://esm.sh/@langchain/openai@0.0.28";
 export type {
   ClientOptions,
   OpenAIChatInput,
-  OpenAIEmbeddingsParams
+  OpenAIEmbeddingsParams,
 } from "https://esm.sh/@langchain/openai@0.0.28";
-export { Zodios, makeApi } from "https://esm.sh/@zodios/core@10.9.6";
+export { makeApi, Zodios } from "https://esm.sh/@zodios/core@10.9.6";
 export type { ZodiosOptions } from "https://esm.sh/@zodios/core@10.9.6";
 export { OpenAIWhisperAudio } from "https://esm.sh/langchain@0.1.34/document_loaders/fs/openai_whisper_audio";
 export { qs };
 import qs from "https://esm.sh/qs@6.12.1";
 export { CloudflareWorkersAIEmbeddings } from "https://esm.sh/@langchain/cloudflare@0.0.4";
 export type { CloudflareWorkersAIEmbeddingsParams } from "https://esm.sh/@langchain/cloudflare@0.0.4";
-
